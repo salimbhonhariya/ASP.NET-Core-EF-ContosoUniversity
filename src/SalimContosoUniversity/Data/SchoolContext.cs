a6e40ef9b06ed1e22920895ec2fd2a44980a08ba
Salim Bhonhariya <sbhonhariya@accuratetechnologies.com>
2016-09-30 16:14:58 -0400

    First Commit

---
+++ src/SalimContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalimContosoUniversity.Models.University;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalimContosoUniversity.Data
+{
+    public class SchoolContext: DbContext
+    {
+            public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+            {
+            }
+
+            public DbSet<Course> Courses { get; set; }
+            public DbSet<Enrollment> Enrollments { get; set; }
+            public DbSet<Student> Students { get; set; }
+        }
+}
